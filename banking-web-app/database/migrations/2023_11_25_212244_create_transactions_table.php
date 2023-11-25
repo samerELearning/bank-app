32e45d8c8fccfc0050af84b047388de5c9ec8e19
samerELearning <samerelearning@outlook.com>
2023-11-25 23:32:34 +0200

    Implemented migration file for Transaction model

---
+++ banking-web-app/database/migrations/2023_11_25_212244_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 10, 2);
+            $table->enum('transaction_type', ['deposit', 'withdraw', 'transfer']);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('from_account_id')->nullable()->constrained('accounts')->onDelete('cascade');
+            $table->foreignId('to_account_id')->nullable()->constrained('accounts')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
