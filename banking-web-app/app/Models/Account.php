434ec39e73440a1408d1695d8f625a95426c69a8
samerELearning <samerelearning@outlook.com>
2023-11-25 21:28:40 +0200

    Implemented the Account model

---
+++ banking-web-app/app/Models/Account.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    protected $table = 'accounts';
+
+    protected $fillable = [
+        'account_number',
+        'balance',
+        'currency',
+        'user_id',
+        'created_at',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
