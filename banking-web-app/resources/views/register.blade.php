ffce478ede9059e3547bed35e9173277b508b150
samerELearning <samerelearning@outlook.com>
2023-11-26 20:52:04 +0200

    Implemented the register view

---
+++ banking-web-app/resources/views/register.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <h2>Register</h2>
+
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required>
+
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
