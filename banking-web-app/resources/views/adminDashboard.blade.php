8a871c785e707fd4ae2dabfb97d03c9a5cb6c62f
samerELearning <samerelearning@outlook.com>
2023-11-29 20:52:13 +0200

    Implemented the admin dashboard view

---
+++ banking-web-app/resources/views/adminDashboard.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            padding-left: 20px; /* Add padding to the left of the body */
+        }
+        h1 {
+            color: blue;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            margin-bottom: 10px;
+        }
+        a {
+            color: green;
+            text-decoration: none;
+            
+        }
+        a:hover {
+            color: black;
+        }
+        button {
+            color: white;
+            background-color: #32CD32;
+            border: none;
+            padding: 5px 10px;
+            text-decoration: none;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: darkgreen;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome {{ Auth::user()->name}}</h1>
+    <ul>
+        <li><a href="">Clients</a></li>
+        <li><a href="">Requests</a></li>
+        <li><a href="">Withdraw</a></li>
+        <li><a href="">Deposit</a></li>
+        <li><a href="">Transfer</a></li>
+        <li>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit">Logout</button>
+            </form>
+        </li>
+    </ul>
+</body>
+</html>
