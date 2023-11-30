8c1cd198f130c5e0725b76ae7c84b73fa2723838
samerELearning <samerelearning@outlook.com>
2023-11-30 16:06:32 +0200

    Implemented styling for the 'Create Account' view

--- banking-web-app/resources/views/create-bank-account.blade.php
+++ banking-web-app/resources/views/create-bank-account.blade.php
@@ -2,6 +2,40 @@
 <html>
 <head>
     <title>Create Bank Account</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f0f0f0;
+            font-family: Arial, sans-serif;
+        }
+        form {
+            background: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+        }
+        h1 {
+            text-align: center;
+            color: blue
+        }
+        label, select, input[type="submit"] {
+            display: block;
+            margin-bottom: 20px;
+        }
+        input[type="submit"] {
+            background: #007BFF;
+            border: none;
+            color: white;
+            padding: 10px 20px;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
     <h1>Create a New Bank Account</h1>
