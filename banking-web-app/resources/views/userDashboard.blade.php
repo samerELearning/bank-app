6b9fc248deadfd8417d3786c0daf963a4336a0f7
samerELearning <samerelearning@outlook.com>
2023-12-02 18:09:09 +0200

    Added route in navigation link for displaying user transaction history

--- banking-web-app/resources/views/userDashboard.blade.php
+++ banking-web-app/resources/views/userDashboard.blade.php
@@ -43,7 +43,7 @@
     <ul>
         <li><a href="{{ route('create.bank.account') }}">Create Bank Account</a></li>
         <li><a href="{{ route('show.bank.accounts') }}">My Bank Accounts</a></li>
-        <li><a href="">My Transactions</a></li>
+        <li><a href="{{ route('show.transaction.history') }}">My Transactions</a></li>
         <li><a href="">Withdraw</a></li>
         <li><a href="">Deposit</a></li>
         <li><a href="">Transfer</a></li>
