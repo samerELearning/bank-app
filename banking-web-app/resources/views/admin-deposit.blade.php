e839c0cf709a4e4f1b01b09e627cb4051323e5d2
samerELearning <samerelearning@outlook.com>
2023-12-06 14:07:48 +0200

    Implemented the admin deposit form view

---
+++ banking-web-app/resources/views/admin-deposit.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Deposit</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f0f0f0;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: blue;
+        }
+        form {
+            background-color: #fff;
+            padding: 100px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+        }
+        label {
+            display: block;
+            margin-bottom: 10px;
+        }
+        input[type="text"], input[type="number"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+        }
+        input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            display: block;
+            margin: auto;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        input[type="submit"]:hover {
+            background-color: darkgreen;
+        }
+        .dashboard-button {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+        .dashboard-button:hover {
+            background-color: darkgreen;
+        }
+    </style>
+</head>
+<body>
+    <a href="{{ route('admin.dashboard') }}" class="dashboard-button">Go to Dashboard</a>
+    <h1>Deposit</h1>
+    <form action="{{ route('show.admin.deposit.form') }}" method="get">
+        @csrf
+        <label for="account_number">Account Number:</label><br>
+        <input type="text" id="account_number" name="account_number"><br>
+        <label for="amount">Amount:</label><br>
+        <input type="number" id="amount" name="amount" min="0.01" step="10"><br>
+        <input type="submit" value="Deposit">
+    </form>
+</body>
+</html>
