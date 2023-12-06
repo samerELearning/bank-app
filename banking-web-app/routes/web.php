f1ae6a6b5d76946cf50c8c7398e066de71fc35f6
samerELearning <samerelearning@outlook.com>
2023-12-06 12:03:50 +0200

    Added route for showing list of users form

--- banking-web-app/routes/web.php
+++ banking-web-app/routes/web.php
@@ -44,4 +44,6 @@ Route::get('/user/show-transaction-history', 'App\Http\Controllers\UserControlle
 Route::get('/user/withdraw', 'App\Http\Controllers\UserController@showWithdrawForm')->name('show.withdraw.form');
 Route::get('/user/deposit', 'App\Http\Controllers\UserController@showDepositForm')->name('show.deposit.form');
 Route::get('/user/transfer', 'App\Http\Controllers\UserController@showTransferForm')->name('show.transfer.form');
+
 //Admin operations routes
+Route::get('/admin/show-users', 'App\Http\Controllers\UserController@showUsers')->name('show.users');
