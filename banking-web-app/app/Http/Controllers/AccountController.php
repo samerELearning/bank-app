99fd427e52f5c33278f659f21e519ebeac305f49
samerELearning <samerelearning@outlook.com>
2023-12-07 18:11:12 +0200

    Created an account controller file

---
+++ banking-web-app/app/Http/Controllers/AccountController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Transaction;
+use App\Models\Account;
+
+class AccountController extends Controller
+{
+    
+}
