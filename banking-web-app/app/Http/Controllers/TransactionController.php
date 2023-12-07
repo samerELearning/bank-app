47e349b6677a5699e8564494fa4891634bb9482e
samerELearning <samerelearning@outlook.com>
2023-12-07 23:31:28 +0200

    Implemented a method for clients to perform transfer transactions

--- banking-web-app/app/Http/Controllers/TransactionController.php
+++ banking-web-app/app/Http/Controllers/TransactionController.php
@@ -105,4 +105,71 @@ class TransactionController extends Controller
 
         return redirect('/user/show-transaction-history')->with('success', 'Deposit successful.');
     }
+
+    /**
+     * Perform a transfer transaction for clients.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function clientTransfer(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'from_account_number' => 'required|exists:accounts,account_number',
+            'to_account_number' => 'required|exists:accounts,account_number',
+            'amount' => 'required|numeric|min:0',
+        ]);
+        
+        if ($validator->fails()) {
+            return redirect()->back()->with(['error' => 'Account not found.']);
+        }
+
+        $from_account = Account::where('account_number', $request->from_account_number)
+                  ->where('user_id', Auth::id())
+                  ->first();
+
+        if (!$from_account) {
+            return redirect()->back()->with(['error' => 'First account not found.']);
+        }
+        if ($from_account->status == 'pending') {
+            return redirect()->back()->with(['error' => 'First account is still pending approval.']);
+        }
+        if ($from_account->status == 'blocked') {
+            return redirect()->back()->with(['error' => 'First account has been blocked.']);
+        }
+        if ($from_account->balance < $request->amount) {
+            return redirect()->back()->with(['error' => 'Insufficient balance in first account.']);
+        }
+
+        $to_account = Account::where('account_number', $request->to_account_number)
+                  ->first();
+
+        if (!$to_account) {
+            return redirect()->back()->with(['error' => 'Second account not found.']);
+        }
+        if ($to_account->status == 'pending') {
+            return redirect()->back()->with(['error' => 'Second account is still pending approval.']);
+        }
+        if ($to_account->status == 'blocked') {
+            return redirect()->back()->with(['error' => 'Second account has been blocked.']);
+        }
+
+        $from_account->balance -= $request->amount;
+        $from_account->save();
+
+        $to_account->balance += $request->amount;
+        $to_account->save();
+
+        // Create a new transaction record
+        $transaction = new Transaction;
+        $transaction->amount = $request->amount;
+        $transaction->transaction_type = 'transfer';
+        $transaction->user_id = $from_account->user_id;
+        $transaction->from_account_id = $from_account->id;
+        $transaction->to_account_id = $to_account->id;
+        $transaction->save();
+
+        return redirect('/user/show-transaction-history')->with('success', 'Transfer successful.');
+    }
 }
